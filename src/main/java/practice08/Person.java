7f61e883cb48355eb596b81680e6a138da430e1b
Joi Liu <547169735@qq.com>
2019-07-11 20:43:49 +0800

    finish practice08

--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,37 @@
 package practice08;
 
+import java.util.Objects;
+
 public class Person {
+  private int id;
+  private String name;
+  private int age;
+
+  public Person(int id, String name, int age) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    Person person = (Person) o;
+    return id == person.id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public String introduce() {
+    String str = "My name is " + name + ". I am " + age + " years old.";
+    return str;
+  }
 }
+
+
+
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,34 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person {
+  private Klass klass;
+  Person person = new Person(1,"Tom", 21);
+
+  public Klass getKlass() {
+    return klass;
+  }
+
+  public Teacher(int id, String name, int age, Klass klass) {
+    super(id, name, age);
+    this.klass = klass;
+  }
+
+  public Teacher(int id, String name, int age) {
+    super(id, name, age);
+  }
+
+  public String introduce() {
+    String str = person.introduce() + " I am a Teacher. I teach ";
+    str = klass == null ? (str + "No Class.") : (str + "Class " + klass.getNumber() + ".");
+    return str;
+  }
+
+  public String introduceWith(Student student) {
+    String str = person.introduce() + " I am a Teacher. I";
+    str+=student.getKlass().getNumber() == klass.getNumber()?" teach ":" don't teach ";
+    str += student.getName() + ".";
+    return str;
+  }
 }
+
+
