f756614bc93b795274271d7cbeb1532bbb136d56
Joi Liu <547169735@qq.com>
2019-07-11 21:15:31 +0800

    finish practice10

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,32 @@
 package practice10;
 
-public class Student {
+public class Student extends Person {
+  private Klass klass;
+  private Student leader;
+
+  public Klass getKlass() {
+    return klass;
+  }
+
+  public Student(int id, String name, int age, Klass klass) {
+    super(id, name, age);
+    this.klass = klass;
+  }
+
+  public String introduce() {
+    Person person = new Person(1, "Tom", 21);
+    String str = person.introduce() + " I am a Student. I am ";
+    if (klass.getLeader() != null && klass.getLeader().getName() == person.getName()) {
+      str += "Leader of ";
+    } else {
+      str += "at ";
+    }
+    str += klass.getDisplayName() + ".";
+    return str;
+  }
 }
+
+
+
+
+
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,56 @@
 package practice10;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person {
+  Person person = new Person(1, "Tom", 21);
+  private LinkedList<Klass> classes = new LinkedList<>();
+
+  public Teacher(int id, String name, int age, LinkedList<Klass> classes) {
+    super(id, name, age);
+    this.classes = classes;
+  }
+
+  public Teacher(int id, String name, int age) {
+    super(id, name, age);
+  }
+
+  public LinkedList<Klass> getClasses() {
+    return classes;
+  }
+
+  public String introduce() {
+    String str = person.introduce() + " I am a Teacher. I teach ";
+    if (classes.size() == 0) {
+      str += "No Class.";
+    } else {
+      str += "Class ";
+      for (Klass klass : classes) {
+        str += klass.getNumber() + ", ";
+      }
+      str = str.substring(0, str.length() - 2);
+      str += ".";
+    }
+    return str;
+  }
+
+  public String introduceWith(Student student) {
+    String str = person.introduce() + " I am a Teacher. I";
+    str += isTeaching(student) ? " teach " : " don't teach ";
+    str += student.getName() + ".";
+    return str;
+  }
+
+  public boolean isTeaching(Student student) {
+    for (Klass klass : classes) {
+      if (klass.isIn(student)) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
+
+
+
+
