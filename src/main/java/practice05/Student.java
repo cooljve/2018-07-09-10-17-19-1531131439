b03784f7b326c4fc56e30eefb10d52fe73fa5574
Joi Liu <547169735@qq.com>
2019-07-11 19:16:17 +0800

    finish practice05

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,20 @@
 package practice05;
 
-public class Student {
+public class Student extends Person {
+  private int klass;
+
+  public int getKlass() {
+    return klass;
+  }
+
+  public Student(String name, int age, int klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  public String introduce(){
+    Person person = new Person("Tom", 21);
+    String str = person.introduce()+" I am a Student. I am at Class "+klass+".";
+    return str;
+  }
 }
