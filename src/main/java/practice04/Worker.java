52290344b003301cd166b26c293a5125da90e088
Joi Liu <547169735@qq.com>
2019-07-11 19:13:58 +0800

    finish practice04

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,14 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person {
+
+  public Worker(String name, int age) {
+    super(name, age);
+  }
+
+  public String introduce() {
+    Person person = new Person("Tom", 21);
+    String str = person.introduce()+" I am a Worker. I have a job.";
+    return str;
+  }
 }
