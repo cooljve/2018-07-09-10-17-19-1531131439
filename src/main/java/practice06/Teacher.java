7698878b053b2bdcad5185fdf0d7321b48af9cc3
Joi Liu <547169735@qq.com>
2019-07-11 20:11:54 +0800

    finish practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,26 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+  private int klass;
+
+  public int getKlass() {
+    return klass;
+  }
+
+  public Teacher(String name, int age, int klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  public Teacher(String name, int age) {
+    super(name, age);
+  }
+
+  public String introduce(){
+    Person person = new Person("Tom", 21);
+
+    String str = person.introduce()+" I am a Teacher. I teach ";
+    str = klass == 0 ? (str + "No Class.") : (str + "Class " + klass+".");
+    return str;
+  }
 }
