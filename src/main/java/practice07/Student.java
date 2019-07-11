9d480a85941ca1b815080ad6b32c52ca7b3f33f6
Joi Liu <547169735@qq.com>
2019-07-11 20:26:38 +0800

    finish practice07

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,22 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+  private Klass klass;
+
+  public Klass getKlass() {
+    return klass;
+  }
+
+  public Student(String name, int age, Klass klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  public String introduce(){
+    Person person = new Person("Tom", 21);
+    String str = person.introduce()+" I am a Student. I am at Class "+klass.getNumber()+".";
+    return str;
+  }
 }
+
+
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,33 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+  private Klass klass;
+  Person person = new Person("Tom", 21);
+
+  public Klass getKlass() {
+    return klass;
+  }
+
+  public Teacher(String name, int age, Klass klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  public Teacher(String name, int age) {
+    super(name, age);
+  }
+
+  public String introduce() {
+    String str = person.introduce() + " I am a Teacher. I teach ";
+    str = klass == null ? (str + "No Class.") : (str + "Class " + klass.getNumber() + ".");
+    return str;
+  }
+
+  public String introduceWith(Student student) {
+    String str = person.introduce() + " I am a Teacher. I";
+    str+=student.getKlass().getNumber() == klass.getNumber()?" teach ":" don't teach ";
+    str += student.getName() + ".";
+    return str;
+  }
 }
+
